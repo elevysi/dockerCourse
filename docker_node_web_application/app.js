b11ab5ae229cef5c9926ce31aa5d0252cd5612c8
elevysi <elevysi@gmail.com>
2019-11-02 11:31:42 +0100

    Ignore Files

--- docker_node_web_application/app.js
+++ docker_node_web_application/app.js
@@ -1,12 +1,12 @@
-//app.js
-// Hello, World! NodeJS Application
-const http = require("http");
-const port = 8080;
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-    res.end("Hello, World!");
+const port = process.env.port || 8081;
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
 });
 
-server.listen(port, () => {
-    console.log("Server is running on http://localhost:%s", port);
+app.listen(port , () => {
+    console.log("Express Web Application running on port %s", port);
 });
