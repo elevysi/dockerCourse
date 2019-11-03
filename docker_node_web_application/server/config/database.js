9ea74c90845f693d068b976753ad5a7c2a2fdcea
elevysi <elevysi@gmail.com>
2019-11-03 19:13:04 +0100

    Init Chapter 3

--- docker_node_web_application/server/config/database.js
+++ docker_node_web_application/server/config/database.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
+const host = process.env.DB_HOST || '127.0.0.1'
 //mongodb://username:password@localhost:port/database
-const dbURL = "mongodb://localhost/dockerAppDB";
+const dbURL = `mongodb://${host}/dockerAppDB`;
 
 const connect = () => {
     setTimeout(() => mongoose.connect(dbURL, { useNewUrlParser: true, useCreateIndex: true }), 1000);
-}
+  }
 
 
 const dbUri = "mongodb://localhost/dockerAppDB";
